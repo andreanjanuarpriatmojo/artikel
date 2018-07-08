b6a1025aeadfb441055ee1b11b76e14557be18c9
unknown <an.jp29@gmail.com>
2018-07-08 19:51:45 +0700

    7/8/2018 - berhasil taruh taruh template views ke laravel

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -12,28 +12,28 @@
   <title>Creative - Bootstrap Admin Template</title>
 
   <!-- Bootstrap CSS -->
-  <link href="{{url('administrator/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{url('admn/css/bootstrap.min.css')}}" rel="stylesheet">
   <!-- bootstrap theme -->
-  <link href="{{url('administrator/css/bootstrap-theme.css')}}" rel="stylesheet">
+  <link href="{{url('admn/css/bootstrap-theme.css')}}" rel="stylesheet">
   <!--external css-->
   <!-- font icon -->
-  <link href="{{url('administrator/css/elegant-icons-style.css')}}" rel="stylesheet" />
-  <link href="{{url('administrator/css/font-awesome.min.css')}}" rel="stylesheet" />
+  <link href="{{url('admn/css/elegant-icons-style.css')}}" rel="stylesheet" />
+  <link href="{{url('admn/css/font-awesome.min.css')}}" rel="stylesheet" />
   <!-- full calendar css-->
-  <link href="{{url('administrator/assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css')}}" rel="stylesheet" />
-  <link href="{{url('administrator/assets/fullcalendar/fullcalendar/fullcalendar.css')}}" rel="stylesheet" />
+  <link href="{{url('admn/assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css')}}" rel="stylesheet" />
+  <link href="{{url('admn/assets/fullcalendar/fullcalendar/fullcalendar.css')}}" rel="stylesheet" />
   <!-- easy pie chart-->
-  <link href="{{url('administrator/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css')}}" rel="stylesheet" type="text/css" media="screen" />
+  <link href="{{url('admn/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css')}}" rel="stylesheet" type="text/css" media="screen" />
   <!-- owl carousel -->
-  <link rel="stylesheet" href="{{url('administrator/css/owl.carousel.css')}}" type="text/css">
-  <link href="{{url('administrator/css/jquery-jvectormap-1.2.2.css')}}" rel="stylesheet">
+  <link rel="stylesheet" href="{{url('admn/css/owl.carousel.css')}}" type="text/css">
+  <link href="{{url('admn/css/jquery-jvectormap-1.2.2.css')}}" rel="stylesheet">
   <!-- Custom styles -->
-  <link rel="stylesheet" href="{{url('administrator/css/fullcalendar.css')}}">
-  <link href="{{url('administrator/css/widgets.css')}}" rel="stylesheet">
-  <link href="{{url('administrator/css/style.css')}}" rel="stylesheet">
-  <link href="{{url('administrator/css/style-responsive.css')}}" rel="stylesheet" />
-  <link href="{{url('administrator/css/xcharts.min.css')}}" rel=" stylesheet">
-  <link href="{{url('administrator/css/jquery-ui-1.10.4.min.css')}}" rel="stylesheet">
+  <link rel="stylesheet" href="{{url('admn/css/fullcalendar.css')}}">
+  <link href="{{url('admn/css/widgets.css')}}" rel="stylesheet">
+  <link href="{{url('admn/css/style.css')}}" rel="stylesheet">
+  <link href="{{url('admn/css/style-responsive.css')}}" rel="stylesheet" />
+  <link href="{{url('admn/css/xcharts.min.css')}}" rel=" stylesheet">
+  <link href="{{url('admn/css/jquery-ui-1.10.4.min.css')}}" rel="stylesheet">
   <!-- =======================================================
     Theme Name: NiceAdmin
     Theme URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
@@ -1061,46 +1061,46 @@
   <!-- container section start -->
 
   <!-- javascripts -->
-  <script src="{{url('administrator/js/jquery.js')}}"></script>
-  <script src="{{url('administrator/js/jquery-ui-1.10.4.min.js')}}"></script>
-  <script src="{{url('administrator/js/jquery-1.8.3.min.js')}}"></script>
-  <script type="text/javascript" src="{{url('administrator/js/jquery-ui-1.9.2.custom.min.js')}}"></script>
+  <script src="{{url('admn/js/jquery.js')}}"></script>
+  <script src="{{url('admn/js/jquery-ui-1.10.4.min.js')}}"></script>
+  <script src="{{url('admn/js/jquery-1.8.3.min.js')}}"></script>
+  <script type="text/javascript" src="{{url('admn/js/jquery-ui-1.9.2.custom.min.js')}}"></script>
   <!-- bootstrap -->
-  <script src="{{url('administrator/js/bootstrap.min.js')}}"></script>
+  <script src="{{url('admn/js/bootstrap.min.js')}}"></script>
   <!-- nice scroll -->
-  <script src="{{url('administrator/js/jquery.scrollTo.min.js')}}"></script>
-  <script src="{{url('administrator/js/jquery.nicescroll.js')}}" type="text/javascript"></script>
+  <script src="{{url('admn/js/jquery.scrollTo.min.js')}}"></script>
+  <script src="{{url('admn/js/jquery.nicescroll.js')}}" type="text/javascript"></script>
   <!-- charts scripts -->
-  <script src="{{url('administrator/assets/jquery-knob/js/jquery.knob.js')}}"></script>
-  <script src="{{url('administrator/js/jquery.sparkline.js')}}" type="text/javascript"></script>
-  <script src="{{url('administrator/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js')}}"></script>
-  <script src="{{url('administrator/js/owl.carousel.js')}}"></script>
+  <script src="{{url('admn/assets/jquery-knob/js/jquery.knob.js')}}"></script>
+  <script src="{{url('admn/js/jquery.sparkline.js')}}" type="text/javascript"></script>
+  <script src="{{url('admn/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js')}}"></script>
+  <script src="{{url('admn/js/owl.carousel.js')}}"></script>
   <!-- jQuery full calendar -->
-  <<script src="{{url('administrator/js/fullcalendar.min.js')}}"></script>
+  <<script src="{{url('admn/js/fullcalendar.min.js')}}"></script>
     <!-- Full Google Calendar - Calendar -->
-    <script src="{{url('administrator/assets/fullcalendar/fullcalendar/fullcalendar.js')}}"></script>
+    <script src="{{url('admn/assets/fullcalendar/fullcalendar/fullcalendar.js')}}"></script>
     <!--script for this page only-->
-    <script src="{{url('administrator/js/calendar-custom.js')}}"></script>
-    <script src="{{url('administrator/js/jquery.rateit.min.js')}}"></script>
+    <script src="{{url('admn/js/calendar-custom.js')}}"></script>
+    <script src="{{url('admn/js/jquery.rateit.min.js')}}"></script>
     <!-- custom select -->
-    <script src="{{url('admin/js/jquery.customSelect.min.js')}}"></script>
-    <script src="{{url('admin/assets/chart-master/Chart.js')}}"></script>
+    <script src="{{url('admn/js/jquery.customSelect.min.js')}}"></script>
+    <script src="{{url('admn/assets/chart-master/Chart.js')}}"></script>
 
     <!--custome script for all page-->
-    <script src="{{url('admin/js/scripts.js')}}"></script>
+    <script src="{{url('admn/js/scripts.js')}}"></script>
     <!-- custom script for this page-->
-    <script src="{{url('admin/js/sparkline-chart.js')}}"></script>
-    <script src="{{url('admin/js/easy-pie-chart.js')}}"></script>
-    <script src="{{url('admin/js/jquery-jvectormap-1.2.2.min.js')}}"></script>
-    <script src="{{url('admin/js/jquery-jvectormap-world-mill-en.js')}}"></script>
-    <script src="{{url('admin/js/xcharts.min.js')}}"></script>
-    <script src="{{url('admin/js/jquery.autosize.min.js')}}"></script>
-    <script src="{{url('admin/js/jquery.placeholder.min.js')}}"></script>
-    <script src="{{url('admin/js/gdp-data.js')}}"></script>
-    <script src="{{url('admin/js/morris.min.js')}}"></script>
-    <script src="{{url('admin/js/sparklines.js')}}"></script>
-    <script src="{{url('admin/js/charts.js')}}"></script>
-    <script src="{{url('admin/js/jquery.slimscroll.min.js')}}"></script>
+    <script src="{{url('admn/js/sparkline-chart.js')}}"></script>
+    <script src="{{url('admn/js/easy-pie-chart.js')}}"></script>
+    <script src="{{url('admn/js/jquery-jvectormap-1.2.2.min.js')}}"></script>
+    <script src="{{url('admn/js/jquery-jvectormap-world-mill-en.js')}}"></script>
+    <script src="{{url('admn/js/xcharts.min.js')}}"></script>
+    <script src="{{url('admn/js/jquery.autosize.min.js')}}"></script>
+    <script src="{{url('admn/js/jquery.placeholder.min.js')}}"></script>
+    <script src="{{url('admn/js/gdp-data.js')}}"></script>
+    <script src="{{url('admn/js/morris.min.js')}}"></script>
+    <script src="{{url('admn/js/sparklines.js')}}"></script>
+    <script src="{{url('admn/js/charts.js')}}"></script>
+    <script src="{{url('admn/js/jquery.slimscroll.min.js')}}"></script>
     <script>
       //knob
       $(function() {
