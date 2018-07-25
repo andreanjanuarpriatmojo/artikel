76312c2a7a656c5cc080fa36ae2fea18390413f9
unknown <an.jp29@gmail.com>
2018-07-25 09:28:48 +0700

    tambah sedikit

--- resources/views/admin/view-artikel.blade.php
+++ resources/views/admin/view-artikel.blade.php
@@ -10,15 +10,15 @@ Admin Page
         <!--overview start-->
         <div class="row">
           <div class="col-lg-12">
-            <h3 class="page-header"><i class="fa fa-eye"></i> View</h3>
+            <h3 class="page-header"><i class="fa fa-eye"></i>View</h3>
           </div>
           <div class="col-lg-8">
               <div class="panel panel-default">
-                  <div class="panel-heading">View</div>
+                  <div class="panel-heading">{{$b->id}}</div>
                   <div class="panel-body">
                       <div class="container">
                         <div style="text-align: justify;">
-                          <h2 >{{$b->title}}</h2>
+                          <h2><b>{{$b->title}}</b></h2>
                           <br>
                           <img src="{{asset('uploads/images')}}/{{$b->photo_path}}" alt="{{$b->photo_path}}" style="height: 400px" class="img-responsive">
                           <br>
