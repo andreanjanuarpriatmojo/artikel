bb00c1d306566b143a543ef24b4ebf3876c59426
unknown <an.jp29@gmail.com>
2018-07-26 12:57:33 +0700

    urutan artikel done

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -10,12 +10,12 @@ class FrontController extends Controller
     //
     public function index()
     {
-        $b = Blog::orderBy('updated_at', 'DESC')->get();
+        $b = Blog::orderBy('id', 'DESC')->get();
         return view('artikel.index',compact('b'));
     }
 
     public function view($id){
-        $bg = Blog::orderBy('updated_at', 'DESC')->get();
+        $bg = Blog::orderBy('id', 'DESC')->get();
     	$blog = Blog::where('id', $id)->first();
     	return view('artikel.artikel', compact('blog','bg'));
     }
