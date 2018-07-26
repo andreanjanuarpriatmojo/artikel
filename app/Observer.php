b65820544b520796cf27092cc4e6bb86a258351c
Ana AZ <naharyana57@gmail.com>
2018-07-26 23:43:22 +0700

    view all artikel

---
+++ app/Observer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Observers;
+
+use App\Blog;
+
+class BlogObserver
+{
+    /**
+     * Listen to the User created event.
+     *
+     * @param  User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        //
+    }
+
+    /**
+     * Listen to the User deleting event.
+     *
+     * @param  User  $user
+     * @return void
+     */
+    public function deleting(User $user)
+    {
+        //
+    }
+}
--- resources/views/artikel/index.blade.php
+++ resources/views/artikel/index.blade.php
@@ -14,6 +14,7 @@
                 </div>
                 <!--End of row-->
                 <div class="row">
+                    @foreach ($b as $bl)
                     <div class="col-md-4">
                         <div class="blog_news">
                             <div class="single_blog_item">
@@ -21,71 +22,26 @@
                                     <img src="{{asset('uploads/images')}}/{{$b[0]->photo_path}}" alt="">
                                 </div>
                                 <div class="blog_content">
-                                    <h3>{{$b[0]->title}}</h3>
+                                    <h3>{{ strip_tags(str_limit($bl->title, 50)) }}</h3>
                                     <div class="expert">
                                         <div class="left-side text-left">
                                             <p class="left_side">
                                                 <span class="clock"><i class="fa fa-clock-o"></i></span>
-                                                <span class="time">{{date('d-m-Y', strtotime($b[0]->updated_at))}}</span>
+                                                <span class="time">{{date('d-m-Y', strtotime($bl->updated_at))}}</span>
                                             </p>
                                         </div>  
                                     </div>
 
-                                    <p class="blog_news_content">{{ strip_tags(str_limit($b[0]->text, 150)) }}</p>
+                                    <p class="blog_news_content">{{ strip_tags(str_limit($bl->text, 150)) }}</p>
                                     <a href="{{ url('blog',['id' => $b[0]->id] )}}" class="blog_link">read more</a>
                                 </div>
                             </div>
                         </div>
+                    <br>
+                    <br>
                     </div>
-                    
-                    <!--End of col-md-4-->
-                    <div class="col-md-4">
-                        <div class="blog_news">
-                            <div class="single_blog_item">
-                                <div class="blog_img">
-                                    <img src="{{asset('uploads/images')}}/{{$b[1]->photo_path}}" alt="">
-                                </div>
-                                <div class="blog_content">
-                                    <h3>{{$b[1]->title}}</h3>
-                                    <div class="expert">
-                                        <div class="left-side text-left">
-                                            <p class="left_side">
-                                                <span class="clock"><i class="fa fa-clock-o"></i></span>
-                                                <span class="time">{{date('d-m-Y', strtotime($b[1]->updated_at))}}</span>
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                    <p class="blog_news_content">{{ strip_tags(str_limit($b[1]->text, 150)) }}</p>
-                                    <a href="{{ url('blog',['id' => $b[1]->id] )}}" class="blog_link">read more</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--End of col-md-4-->
-                    <div class="col-md-4">
-                        <div class="blog_news">
-                            <div class="single_blog_item">
-                                <div class="blog_img">
-                                    <img src="{{asset('uploads/images')}}/{{$b[2]->photo_path}}" alt="">
-                                </div>
-                                <div class="blog_content">
-                                    <h3>{{$b[2]->title}}</h3>
-                                    <div class="expert">
-                                        <div class="left-side text-left">
-                                            <p class="left_side">
-                                                <span class="clock"><i class="fa fa-clock-o"></i></span>
-                                                <span class="time">{{date('d-m-Y', strtotime($b[2]->updated_at))}}</span>
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                    <p class="blog_news_content">{{ strip_tags(str_limit($b[2]->text, 150)) }}</p>
-                                    <a href="{{ url('blog',['id' => $b[2]->id] )}}" class="blog_link">read more</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
+                </div>
                     <!--End of col-md-4-->
                 </div>
                 <!--End of row-->
