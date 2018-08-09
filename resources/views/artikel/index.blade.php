14b72c0cc111e2d4834eda19e077929ae7cf2c8d
unknown <an.jp29@gmail.com>
2018-08-09 13:03:04 +0700

    comments

--- resources/views/artikel/index.blade.php
+++ resources/views/artikel/index.blade.php
@@ -19,12 +19,12 @@
                     <div class="col-md-6 zero_mp">
                         <div class="Kegiatan_item">
                             <div class="Kegiatan_text text-left">
-                                <h4>{{$data->title}}</h4></a>
+                                <h3>{{$data->title}}</h3>
                                 <p>
                                     <span class="clock"><i class="fa fa-clock-o"></i></span>
                                     <span class="time">{{date('d-m-Y', strtotime($data->updated_at))}}</span>
                                 </p>
-                                <p>{{ strip_tags(str_limit($data->text, 80)) }}</p>
+                                <p>{!! strip_tags(str_limit($data->text, 80)) !!}</p>
                                 <br>
                                 <a href="{{ url('blog',['id' => $data->id] )}}" class="Kegiatan_btn">read more</a>
                             </div>
@@ -50,7 +50,7 @@
                             </div>
                             <div class="Kegiatan_news_text">
                                 <a href="{{ url('blog',['id' => $blog->id] )}}"><h4>{{$blog->title}}</h4></a>
-                                <p>{{ strip_tags(str_limit($blog->text, 30)) }}</p>
+                                <p>{!! strip_tags(str_limit($blog->text, 60)) !!}</p>
                             </div>
                         </div>
                     </div>
@@ -73,7 +73,7 @@
                             </div>
                             <div class="Kegiatan_news_text">
                                 <a href="{{ url('blog',['id' => $pop->id] )}}"><h4>{{$pop->title}}</h4></a>
-                                <p>{{ strip_tags(str_limit($pop->text, 30)) }}</p>
+                                <p>{!! strip_tags(str_limit($pop->text, 60)) !!}</p>
                             </div>
                         </div>
                     </div>
